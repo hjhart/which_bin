1b33040e73773c46cc4af3d6ffe69f339a470291
James Hart <hjhart@gmail.com>
2013-07-11 13:22:17 -0700

    rails project shell checkin

---
+++ ios/www/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WhichBinIos::Application.config.secret_key_base = 'e6224852a8b06f73f5a2680e147ba402324464b2f4a49c4543732e8d0b1d52e8a68aef5ac1e265c4ed39a72e1521efb34e4bd9c2680294cec76236fcba7a6b00'
---
+++ ios/www/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WhichBinIos::Application.config.session_store :cookie_store, key: '_which_bin_ios_session'
